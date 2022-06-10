24894e82976e91071e52d030ae74982455010c14
AdityRaj99 <adityaraj9983@gmail.com>
2022-06-10 21:35:47 +0530

    config

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = ()=>{
-    return mongoose.connect("mongodb+srv://webdevesh:devesh@cluster0.wsim0.mongodb.net/ecom?retryWrites=true&w=majority");
+    return mongoose.connect("mongodb://aditya:aditya123@cluster0-shard-00-00.2utw4.mongodb.net:27017,cluster0-shard-00-01.2utw4.mongodb.net:27017,cluster0-shard-00-02.2utw4.mongodb.net:27017/?ssl=true&replicaSet=atlas-8g8fh7-shard-0&authSource=admin&retryWrites=true&w=majority");
 };
